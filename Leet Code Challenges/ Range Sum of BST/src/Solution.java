1504f9c79fc365e8f3e111d8f0c880fcc47f4e6a
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2020-08-20 14:32:27 +0200

    Add files via upload

---
+++ Leet Code Challenges/ Range Sum of BST/src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null) return 0;
+        if(root.val >= L && root.val<=R){
+            return root.val + rangeSumBST(root.left,L,R) + rangeSumBST(root.right,L,R) ;
+        }else {
+            return rangeSumBST(root.left,L,R) + rangeSumBST(root.right,L,R) ;
+        }
+    }
+
+
+
+}
