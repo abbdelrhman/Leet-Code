67a9c8abb213ea4ad0cfcce51d0720f16948dc12
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2020-08-21 17:40:18 +0200

    Add files via upload

---
+++ Leet Code Challenges/ N-ary Tree Preorder Traversal/src/Node.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
+class Solution {
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorder(Node root) {
+        if (root == null) {
+            return list;
+        } else {
+            list.add(root.val);
+            for (Node n : root.children) {
+                preorder(n);
+            }
+        }
+        return list;
+    }
+}
