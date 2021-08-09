1cc66213c07d2f7f746ff97f4bbc930918e90ce0
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2021-08-09 17:24:17 +0200

    Add files via upload

---
+++ Leet Code Challenges/Climbing Stairs/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int climbStairs(int n) {
+        if (n == 0) return 0;
+        int i = 1, fn2 = 1, fn1 = 1, fn = 1;
+        while (i++ < n){
+            fn = fn1 + fn2;
+            fn2 = fn1;
+            fn1 = fn;
+        }
+        return fn;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));
+    }
+}
