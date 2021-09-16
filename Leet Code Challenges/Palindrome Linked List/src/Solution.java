3da25e70b8b13ff82f7b0272a6e82734275c4ece
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2021-09-16 18:09:45 +0200

    Add files via upload

--- Leet Code Challenges/Palindrome Linked List/src/Solution.java
+++ Leet Code Challenges/Palindrome Linked List/src/Solution.java
@@ -1,37 +1,40 @@
 import java.util.ArrayList;
 
 class Solution {
-    public static boolean isPalindrome(ListNode head) {
-        ListNode curr = head ;
-        ArrayList<Integer> saved = new ArrayList<>();
-        ListNode prev = null ,  next = null;
-        while (curr!=null){
-            saved.add(curr.val);
-            next = curr.next;
-            curr.next=prev;
-            prev=curr;
-            
-            curr=next;
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        int i=0;
-        while (prev!=null && i<saved.size()){
-            if (saved.get(i) == prev.val){
-                prev=prev.next;
-                i++;
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
 
-            }else{
-               
+        while (slow != null) {
+            if (fast.val != slow.val) {
                 return false;
             }
+            fast = fast.next;
+            slow = slow.next;
         }
         return true;
     }
 
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next= new ListNode(1);
-        head.next.next= new ListNode(2);
-        head.next.next.next= new ListNode(1);
-        System.out.println(isPalindrome(head));
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
+
+
+
+
 }
