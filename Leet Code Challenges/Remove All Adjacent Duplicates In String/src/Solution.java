3a3a8f98034722ef3ab840ebbee5800a6ddfbeb2
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2021-10-06 14:21:42 +0200

    Add files via upload

---
+++ Leet Code Challenges/Remove All Adjacent Duplicates In String/src/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.peek() == s.charAt(i)) {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        String ans = "";
+        while (!stack.isEmpty()) {
+            ans = stack.pop() + ans;
+        }
+        return ans;
+    }
+}
+
+/*
+StringBuilder as stack
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
+ */
