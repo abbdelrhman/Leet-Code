02d779302ed7ef0427d63491dbd59815e180ccea
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2021-10-10 11:23:35 +0200

    Add files via upload

---
+++ Leet Code Challenges/Implement Queue using Stacks/src/MyQueue.java
@@ -0,0 +1,50 @@
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> pushStack = new Stack<>();
+    private Stack<Integer> popStack = new Stack<>();
+    int top = 0;
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        if (pushStack.isEmpty() && popStack.isEmpty()) {
+            top = x;
+        } else if (pushStack.isEmpty() && !popStack.isEmpty()) {
+            top = popStack.peek();
+            while (!popStack.isEmpty()) {
+                pushStack.push(popStack.pop());
+            }
+
+        }
+        pushStack.push(x);
+    }
+
+    public int pop() {
+        while (!pushStack.isEmpty()) {
+            popStack.push(pushStack.pop());
+        }
+        int ans= popStack.pop();
+        top= popStack.isEmpty()?-1:popStack.peek();
+        return ans;
+    }
+
+    public int peek() {
+        return top;
+    }
+
+    public boolean empty() {
+        return pushStack.isEmpty() && popStack.isEmpty() ? true : false;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
